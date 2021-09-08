ad60bd72fd2979d71e2d286ee7fff3563be381e9
⌫ <shmn7iii@gmail.com>
2021-09-08 14:46:27 +0900

    add host to whitelist

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -110,4 +110,7 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  # host whitelist
+  config.hosts << "api.tap.shmn7iii.net"
 end
