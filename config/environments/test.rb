2751af70248f8f7aac82c6add564e157de3ed602
⌫ <shmn7iii@gmail.com>
2021-09-10 13:36:44 +0900

    setup for for nginx
    for nginx

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -57,4 +57,7 @@ Rails.application.configure do
 
   # Annotate rendered view with file names.
   # config.action_view.annotate_rendered_view_with_filenames = true
+
+  # host whitelist
+  config.hosts << "api.tap.shmn7iii.net"
 end
