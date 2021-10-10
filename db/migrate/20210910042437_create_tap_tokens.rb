84cc36b4d96e55e49db12b4e70a06014d14c982c
⌫ <shmn7iii@gmail.com>
2021-10-10 14:07:09 +0900

    change TapToken table
    data -> tx_id

--- db/migrate/20210910042437_create_tap_tokens.rb
+++ db/migrate/20210910042437_create_tap_tokens.rb
@@ -2,7 +2,7 @@ class CreateTapTokens < ActiveRecord::Migration[6.1]
   def change
     create_table :tap_tokens, id: false do |t|
       t.string :token_id, primary_key: true
-      t.string :data
+      t.string :tx_id
       t.timestamps
     end
   end
