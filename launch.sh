6ae134f26c19a3495a42686167333d6f8b448fb7
⌫ <shmn7iii@gmail.com>
2021-11-04 10:50:48 +0900

    move development mode

--- launch.sh
+++ launch.sh
@@ -8,4 +8,5 @@ docker start 70ca731e4c53
 ipfs daemon &
 
 # Rails
-/root/.rbenv/versions/3.0.1/bin/bundle exec /opt/rails/tap-api/bin/rails s -e production
+# /root/.rbenv/versions/3.0.1/bin/bundle exec /opt/rails/tap-api/bin/rails s -e production
+/root/.rbenv/versions/3.0.1/bin/bundle exec /opt/rails/tap-api/bin/rails s
