db149b4d4462246f76e67778a2a2bc3eb93ec828
shima <shmn7iii@gmail.com>
2021-11-19 15:19:44 +0900

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://tap-f4f38.web.app/', /\Ahttps:\/\/.*.shmn7iii.net\z/
+    origins 'https://tap-f4f38.web.app', /\Ahttps:\/\/.*.shmn7iii.net\z/
 
     resource '*',
       headers: :any,
